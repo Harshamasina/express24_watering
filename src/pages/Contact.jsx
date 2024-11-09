eb9364eb843d3e34d034eee584dfafcb54ae737a
Harshamasina <harshamasina1999@gmail.com>
2024-11-09 12:43:01 -0600

    added: "Alert Message"

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,15 +14,45 @@ const Contact = () => {
         email: "",
         phone: "",
         service: "",
-        message: ""
+        message: "",
+        checkbox_1: false,
+        checkbox_2: false
     });
 
-    let name, value;
-    let handleInputs = (e) => {
-        name=e.target.name;
-        value = e.target.value;
-        setUserMessage({...userMessage, [name]:value});
+    const handleInputs = (e) => {
+        const { name, value, type, checked } = e.target;
+        if (type === "checkbox") {
+            setUserMessage({
+              ...userMessage,
+              checkbox_1: name === "checkbox_1" ? checked : false,
+              checkbox_2: name === "checkbox_2" ? checked : false,
+            });
+          } else {
+            setUserMessage({ ...userMessage, [name]: value });
+        }
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!userMessage.checkbox_1 && !userMessage.checkbox_2) {
+          alert("Please select either 'Yes' or 'No' for text message permission.");
+          return;
+        }
+        // Handle form submission logic here
+        console.log("Form submitted successfully:", userMessage);
+        alert("Thanks for sending us a Message. We will reach you out soon.");
+        setUserMessage({
+            name: "",
+            email: "",
+            phone: "",
+            service: "",
+            message: "",
+            checkbox_1: false,
+            checkbox_2: false,
+        }); 
+    };
+
+    console.log(userMessage);
 
     return (
         <>
@@ -56,7 +86,7 @@ const Contact = () => {
 
                     <div className='contact_form'>
                         <h4>Send Us a Message</h4>
-                        <form action=''>
+                        <form onSubmit={handleSubmit}>
                             <div className="input_box">
                                 <input type='text' placeholder='Enter your Name' name='name' value={userMessage.name} onChange={handleInputs} required/>
                             </div>
@@ -81,6 +111,16 @@ const Contact = () => {
 
                             <div className="input_box">
                                 <textarea type='text' cols="30" rows="10" placeholder='Enter your Message' name='message' value={userMessage.message} onChange={handleInputs} required></textarea>
+                            </div>
+
+                            <div className="checkbox">
+                                <input className='checkbox_input' type='checkbox' id="checkbox_1" name='checkbox_1' checked={userMessage.checkbox_1} onChange={handleInputs} />
+                                <label for="checkbox_1">Yes, I agree to receive text messages from Express 24 Plumbing and Electrical at the phone number listed above</label>
+                            </div>
+
+                            <div className="checkbox">
+                                <input className='checkbox_input' type='checkbox' id="checkbox_2" name='checkbox_2' checked={userMessage.checkbox_2} onChange={handleInputs} />
+                                <label for="checkbox_2">No, I do not want to receive text messages from Express 24 Plumbing and Electrical</label>
                             </div>
 
                             <input className='input_button' type='submit' value="Send Us Message" />
